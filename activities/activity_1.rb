4882a3a663252e6f33e84654a4950bce4f3ba164
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-08-19 20:36:53 +0800

    Activity 1: This function will take the ages that are stored in array where it will take to sum to get the average

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,18 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def age_average(ages)
+  age_sum = 0
+  array_count = ages.count
+
+  ages.each do |indiv_age|
+    age_sum += indiv_age
+  end
+
+  age_ave = age_sum / array_count.to_f
+  return age_ave
+
+end
+
+puts age_average([4, 23, 25, 30, 39, 10])
