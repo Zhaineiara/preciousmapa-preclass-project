3c7c060ebf79bdb6e66610d88a44ade84ef4bf7b
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-08-19 21:02:49 +0800

    Activity 2: This function will display the fullname by combining the values from hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def name(list_name)
+
+  fullname = ""
+  list_name.each do |key, take_names|
+    fullname += take_names  + " "
+  end
+
+  return fullname
+end
+
+puts name(firstname:"Precious Daniella", midname:"Dideles", lastname:"Mapa")
