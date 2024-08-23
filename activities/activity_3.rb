58bd60fec8dfc5f062083eba8560461e282d3b7b
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-08-23 18:07:13 +0800

    Activity 3: This function will take the differences of ages between 1st and 2nd, 2nd and 3rd, and so on. Then it will store in another array to and display the output

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,23 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def given_ages(ages)
+  age_diff = []
+  for num_count in 0...(ages.length-1)
+    age1 = ages[num_count]
+    age2 = ages[num_count+1]
+
+    if (age1 > age2) 
+      age_minus = age1 - age2
+    else 
+      age_minus = age2 - age1
+    end
+    age_diff.push(age_minus)
+  end
+
+  print "Array of Age Differences: #{age_diff}"
+
+end
+
+puts given_ages([10, 10, 15, 5, 3, 1])
