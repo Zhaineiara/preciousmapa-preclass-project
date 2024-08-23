41a5697c6c78418d3386d83cc9aa48c8ce150ef9
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-08-23 18:28:10 +0800

    Activity 4: This function of codes will display the array of ages that appears once in the given array of ages

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,20 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+
+def given_ages(ages)
+  appear_once = []
+
+  for num_count in 0...(ages.length)
+    age_repeat = ages.count(ages[num_count])
+    
+    if age_repeat == 1
+      appear_once.push(ages[num_count])
+    end
+  end
+
+  print "Array of Ages that Appears Once: #{appear_once}"
+
+end
+
+puts given_ages([10, 10, 3, 5, 1, 5, 3, 1, 7, 2])
